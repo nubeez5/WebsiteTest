f191afc12902ca5d8a1164031bffae3c9ac9cb25
henrynguyen2163 <31243529+nubeez5@users.noreply.github.com>
2020-06-01 22:12:08 +1000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+function GoToHomePage() {
+	window.location.href = "/Home/Index.html"
+}
+
+function GoToAboutPage() {
+	window.location.href = "/About/Index.html"
+}
+
+function GoToContactPage() {
+	window.location.href = "/Contact/Index.html"
+}
+
+// Sections
+function GoToWhatIsProgrammingPage() {
+	window.location.href = "/Sections/Introduction/What-Is-Programming/Index.html"
+}
+
+function GoToChoosingALanguagePage() {
+	window.location.href = "/Sections/Introduction/Choosing-A-Language/Index.html"
+}
+
+function GoToDataTypesPage() {
+	window.location.href = "/Sections/Unit-1/Data-Types/Index.html"
+}
+
+function GoToOperationsPage() {
+	window.location.href = "/Sections/Unit-1/Operations/Index.html"
+}
